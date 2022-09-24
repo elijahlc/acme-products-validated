0fa6ee88f178a48d1db8d3cb2879bad0305de62e
Eli Cohen <cohen.elijahlev@gmail.com>
2022-09-24 09:39:33 -0400

    created products listing

--- src/App.js
+++ src/App.js
@@ -2,26 +2,29 @@ import React, { useEffect } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProducts } from './store';
+import Products from './Products';
 
-const App = ()=> {
-  const { orders, products } = useSelector(state => state);
-  const dispatch = useDispatch();
-  useEffect(()=> {
-    dispatch(fetchProducts());
-  }, []);
-  return (
-    <div>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/products'>Products ({ products.length })</Link>
-        <Link to='/orders'>Orders ({ orders.length })</Link>
-      </nav>
-      <Routes>
-        <Route path='/' element={ <div>Home</div> } />
-        <Route path='/products' element={ <div>Products</div> } />
-      </Routes>
-    </div>
-  );
+const App = () => {
+	const { orders, products } = useSelector((state) => state);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchProducts());
+	}, []);
+
+	return (
+		<div>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/products">Products ({products.length})</Link>
+				<Link to="/orders">Orders ({orders.length})</Link>
+			</nav>
+			<Routes>
+				<Route path="/" element={<div>Home</div>} />
+				<Route path="/products" element={<Products />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -3,37 +3,37 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import axios from 'axios';
 
-const products = (state = [], action)=> {
-  if(action.type === 'SET_PRODUCTS'){
-    return action.products;
-  }
-  return state;
-}
-
-const orders = (state = [], action)=> {
-  if(action.type === 'SET_ORDERS'){
-    return action.orders;
-  }
-  return state;
-}
-
-const setProducts = products => {
-  return {
-    type: 'SET_PRODUCTS',
-    products
-  };
+const products = (state = [], action) => {
+	if (action.type === 'SET_PRODUCTS') {
+		return action.products;
+	}
+	return state;
 };
 
-export const fetchProducts = ()=> {
-  return async(dispatch)=> {
-    const response = await axios.get('/api/products');
-    dispatch(setProducts(response.data));
-  };
+const orders = (state = [], action) => {
+	if (action.type === 'SET_ORDERS') {
+		return action.orders;
+	}
+	return state;
+};
+
+const setProducts = (products) => {
+	return {
+		type: 'SET_PRODUCTS',
+		products,
+	};
+};
+
+export const fetchProducts = () => {
+	return async (dispatch) => {
+		const response = await axios.get('/api/products');
+		dispatch(setProducts(response.data));
+	};
 };
 
 const reducer = combineReducers({
-  products,
-  orders
+	products,
+	orders,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
